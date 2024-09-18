bed707dcb1b794696942c61fa60770052310a660
Riley Honbo <riley.honbo@gmail.com>
2024-09-17 17:17:53 -0700

    Adding Posts functionality

---
+++ posts/directory.js
@@ -0,0 +1,12 @@
+var posts = [
+	{
+		"title": "Post 1",
+		"href": "posts/post1/post1.html",
+		"desc": "Something new"
+	},
+	{
+		"title": "Post 2",
+		"href": "posts/post1/post2.html",
+		"desc": "Something new 2"
+	}
+	]
